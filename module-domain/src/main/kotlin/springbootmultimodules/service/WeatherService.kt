ca33cc79722c9a08d30f71d5896383735bed7932
Kyo Jin <137484097+9817kg@users.noreply.github.com>
2024-03-12 20:40:59 +0900

    Update WeatherService.kt

--- module-domain/src/main/kotlin/springbootmultimodules/service/WeatherService.kt
+++ module-domain/src/main/kotlin/springbootmultimodules/service/WeatherService.kt
@@ -10,19 +10,10 @@ import org.springframework.transaction.annotation.Transactional
 @Service
 @Transactional
 class WeatherService(private val weatherRepository: WeatherRepository) {
-
-
-
     fun findById(id: Long): Region? {
         return weatherRepository.findById(id).orElse(null)
     }
-
     fun save(region: Region): Region {
         return weatherRepository.save(region)
     }
-
-
-
-
-
 }
